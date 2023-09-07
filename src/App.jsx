46dbe02f0b2c103ab446468dd74fc9b4c3ac0991
RajxMihir <117967315+RajxMihir@users.noreply.github.com>
2023-09-07 19:45:57 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+// import Card from './Card/Card'
+import Grid from './Components/Icons/Grid/Grid'
+
+function App() {
+  
+
+  return (
+    <>
+      <Grid numberOfCards={9}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Card/Card.jsx
@@ -0,0 +1,19 @@
+import Icon from "../Components/Icons/Icon";
+import './Card.css';
+
+function Card({gameEnd,player, onPlay, index}) {
+    let icon = <Icon />
+    if(player == 'X') {
+        icon = <Icon name="cross" />
+    }
+    else if(player == '0') {
+        icon = <Icon name="circle" />
+    }
+    return (
+        <div className='card' onClick={() => !gameEnd && player=="" && onPlay(index)}>
+            {icon}
+        </div>
+    )
+}
+
+export default Card;
